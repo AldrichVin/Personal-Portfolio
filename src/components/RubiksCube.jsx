150cc4515bbab8a5ec84effa2ed02ece06207bba
AldrichVin <aldrichvin040205@gmail.com>
2026-01-19 20:39:55 +0700

    style: Lower cube position slightly (y: 1.5 -> 0.8)

--- src/components/RubiksCube.jsx
+++ src/components/RubiksCube.jsx
@@ -276,7 +276,7 @@ const Scene = ({ phase, scrollProgress, groupYOffset }) => {
       <pointLight position={[-3, -3, 3]} intensity={0.2} color={COLORS.secondary} />
 
       {/* Position the cube group aligned with hero text - higher and more right */}
-      <group position={[4.5, 1.5, 0]} scale={1.4}>
+      <group position={[4.5, 0.8, 0]} scale={1.4}>
         <Float
           speed={1.5}
           rotationIntensity={0.15}
